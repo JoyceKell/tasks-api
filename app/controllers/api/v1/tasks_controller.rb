88a4f564485235a1c92f5221160b76c0f6365825
Joyce Kelly <joyccekelly5.0@gmail.com>
2022-06-24 08:32:27 -0300

    adicionando rota POST

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -11,6 +11,20 @@ module Api
 				task = Task.find(params[:id])
 				render json: {status: 'SUCCESS', message:'Loaded task', data:task},status: :ok
 			end
+
+			def create
+				task = Task.new(task_parameters)
+				if task.save
+					render json: {status: 'SUCCESS', message:'Saved task', data:task},status: :ok
+				else
+					render json: {status: 'ERROR', message:'Articles not saved', data:task.erros},status: :unprocessable_entity
+				end
+			end
+
+			private
+			def task_parameters
+				params.permit(:name, :url_image, :status, :description)
+			end
 		end
 	end
 end
